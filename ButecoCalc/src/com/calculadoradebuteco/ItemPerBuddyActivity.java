4becc6b9194e155570d68c4be585bd7511b7e630
betito.oliveira@gmail.com <betito.oliveira@gmail.com@049d96ce-48fd-6cc9-8b01-b7ceffaa3ec7>
2013-07-13 05:11:25 +0000

    Bind to assign the items per Buddy.

--- ButecoCalc/src/com/calculadoradebuteco/ItemPerBuddyActivity.java
+++ ButecoCalc/src/com/calculadoradebuteco/ItemPerBuddyActivity.java
@@ -1,35 +1,169 @@
 package com.calculadoradebuteco;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Iterator;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ItemPerBuddyActivity extends Activity {
+import com.calculadoradebuteco.core.DataItem;
+import com.calculadoradebuteco.model.CalcButeco;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_per_buddy);
-//        getActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+public class ItemPerBuddyActivity extends Activity implements OnClickListener {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_item_per_buddy, menu);
-        return true;
-    }
+	TextView itemInfo = null;
+	Button btnSave = null;
+	private ExpandListAdapter ExpAdapter;
+	private ArrayList<ExpandListGroup> ExpListItems;
+	private ExpandableListView ExpandList;
 
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case android.R.id.home:
-//                NavUtils.navigateUpFromSameTask(this);
-//                return true;
-//        }
-        return super.onOptionsItemSelected(item);
-    }
+	Hashtable<String, Integer> Buddy = null;
+	Hashtable<String, Integer> Item = null;
+	Hashtable<String, DataItem> ItemD = null;
+	int X_ = 1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_item_per_buddy);
+		// getActionBar().setDisplayHomeAsUpEnabled(true);
+
+		ExpandList = (ExpandableListView) findViewById(R.id.expList);
+		ExpListItems = SetStandardGroups();
+		ExpAdapter = new ExpandListAdapter(ItemPerBuddyActivity.this,
+				ExpListItems);
+		ExpandList.setAdapter(ExpAdapter);
+
+		CalcButeco.getInstance().setMatrix();
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_item_per_buddy, menu);
+		return true;
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		itemInfo = (TextView) findViewById(R.id.text_information);
+		btnSave = (Button) findViewById(R.id.btn_save);
+
+		btnSave.setOnClickListener(this);
+
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return super.onOptionsItemSelected(item);
+	}
+
+	public ArrayList<ExpandListGroup> SetStandardGroups() {
+
+		ArrayList<ExpandListGroup> list = new ArrayList<ExpandListGroup>();
+
+		if (CalcButeco.getInstance().getBuddyList_DB() != null) {
+
+			String[] listOfBuddies = getListInString(CalcButeco.getInstance()
+					.getBuddyList_DB());
+			String[] listOfItems = getListInString(CalcButeco.getInstance()
+					.getItemListDB());
+
+			for (int i = 0; i < listOfBuddies.length; i++) {
+
+				ArrayList<ExpandListChild> list2 = new ArrayList<ExpandListChild>();
+				ExpandListGroup parent = new ExpandListGroup();
+				parent.setName(listOfBuddies[i]);
+
+				Log.i("PARENT", listOfBuddies[i]);
+
+				for (int j = 0; j < listOfItems.length; j++) {
+
+					ExpandListChild child = new ExpandListChild();
+					child.setName(listOfItems[j]);
+					child.setTag(null);
+					list2.add(child);
+					Log.i("CHILD", listOfItems[j]);
+
+				}
+				parent.setItems(list2);
+				list.add(parent);
+				Log.i("LOOP", "" + i);
+
+				printList(list);
+			}
+
+			return list;
+		}
+
+		return null;
+
+	}
+
+	private void printList(ArrayList<ExpandListGroup> list) {
+		for (Iterator iterator = list.iterator(); iterator.hasNext();) {
+			ExpandListGroup expandList = (ExpandListGroup) iterator.next();
+
+			Log.i("RETURN 1", expandList.getName());
+
+			ArrayList<ExpandListChild> listc = expandList.getItems();
+			for (Iterator iterator2 = listc.iterator(); iterator2.hasNext();) {
+				ExpandListChild expandListChild = (ExpandListChild) iterator2
+						.next();
+
+				Log.i("RETURN 2", expandListChild.getName());
+
+			}
+
+		}
+
+	}
+
+	private String[] getListInString(final Hashtable hashtable) {
+
+		StringBuilder list = new StringBuilder();
+		if (hashtable != null) {
+			for (Enumeration<Object> enx = hashtable.keys(); enx
+					.hasMoreElements();) {
+				String x = (String) enx.nextElement();
+				list.append(x + "::");
+			}
+		} else {
+			list.append("vazio");
+		}
+		return list.toString().split("::");
+	}
+
+	@Override
+	public void onClick(View v) {
+		int id = v.getId();
+
+		switch (id) {
+		case R.id.btn_save:
+			saveMatrix();
+			break;
+		}
+
+	}
+
+	private void saveMatrix() {
+
+		Toast.makeText(getApplicationContext(), "Salvar na matrix",
+				Toast.LENGTH_LONG).show();
+
+	}
 
 }
--- ButecoCalc/src/com/calculadoradebuteco/model/CalcButeco.java
+++ ButecoCalc/src/com/calculadoradebuteco/model/CalcButeco.java
@@ -8,6 +8,9 @@ public class CalcButeco {
 
 	private Hashtable<String, DataItem> itemListDB = null;
 	private Hashtable<String, Integer> buddyList_DB = null;
+	private int Matrix[][] = null;
+	private int BUDDY_ID_COUNT = 0;
+	private int ITEM_ID_COUNT = 0;
 
 	private static CalcButeco instance;
 
@@ -34,6 +37,49 @@ public class CalcButeco {
 
 	public void setBuddyList_DB(Hashtable<String, Integer> buddyList_DB) {
 		this.buddyList_DB = buddyList_DB;
+	}
+
+	public int getBUDDY_ID_COUNT() {
+		return BUDDY_ID_COUNT;
+	}
+
+	public void setBUDDY_ID_COUNT(int bUDDY_ID_COUNT) {
+		BUDDY_ID_COUNT = bUDDY_ID_COUNT;
+	}
+
+	public void inc_BUDDY_ID_COUNT() {
+		this.BUDDY_ID_COUNT++;
+	}
+
+	public void inc_ITEM_ID_COUNT() {
+		this.ITEM_ID_COUNT++;
+	}
+
+	public int getITEM_ID_COUNT() {
+		return ITEM_ID_COUNT;
+	}
+
+	public void setITEM_ID_COUNT(int iTEM_ID_COUNT) {
+		ITEM_ID_COUNT = iTEM_ID_COUNT;
+	}
+
+	public int[][] getMatrix() {
+		return Matrix;
+	}
+
+	public void setMatrix() {
+
+		if ((this.getItemListDB() != null) && (this.getBuddyList_DB() != null)) {
+			int item_size = this.getItemListDB().size();
+			int buddy_size = this.getBuddyList_DB().size();
+			Matrix = new int[item_size][buddy_size];
+
+			for (int x = 0; x < item_size; x++) {
+				for (int y = 0; y < buddy_size; y++) {
+					Matrix[x][y] = 0;
+				}
+			}
+		}
 	}
 
 }
