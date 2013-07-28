f36607a16f5ce37f6ea2d7531c9e5304dbfae66a
betito.oliveira@gmail.com <betito.oliveira@gmail.com@049d96ce-48fd-6cc9-8b01-b7ceffaa3ec7>
2013-07-28 16:48:33 +0000

    Real 1.0 Version.

---
+++ tags/ButecoCalc/src/com/calculadoradebuteco/BuddyActivity.java
@@ -0,0 +1,149 @@
+package com.calculadoradebuteco;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.calculadoradebuteco.model.CalcButeco;
+
+public class BuddyActivity extends Activity implements OnClickListener {
+
+	private Button buttonSave = null;
+	private Button buttonClose = null;
+	private EditText buddyName = null;
+	private TextView buddyList = null;
+
+	private int ID_ = 0;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_buddy);
+
+		if (CalcButeco.getInstance().getBuddyList_DB() == null) {
+			CalcButeco.getInstance().setBuddyList_DB(
+					new Hashtable<String, Integer>());
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_buddy, menu);
+		return true;
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		buttonSave = (Button) findViewById(R.id.button_buddy_save);
+		buttonClose = (Button) findViewById(R.id.button_buddy_close);
+		buddyName = (EditText) findViewById(R.id.edit_buddy_name);
+		buddyList = (TextView) findViewById(R.id.text_buddy_list);
+
+		buttonSave.setOnClickListener(this);
+		buttonClose.setOnClickListener(this);
+
+		updateDisplayBuddyList();
+
+	}
+
+	@Override
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+		case R.id.button_buddy_save:
+			showMsg("Salvar...");
+			writeBuddyInList();
+			ClearFields();
+			break;
+
+		case R.id.button_buddy_close:
+			showMsg("Já...");
+			finish();
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
+	private void ClearFields() {
+
+		buddyName.setText("");
+		buddyName.requestFocus();
+
+	}
+
+	private boolean writeBuddyInList() {
+
+		String name = this.buddyName.getText().toString();
+
+		if (CalcButeco.getInstance().getBuddyList_DB().containsKey(name)) {
+			WarnAlredyExists();
+
+		} else {
+
+			if (name.trim().equals("") == false) {
+				name = name.toUpperCase();
+				ID_ = CalcButeco.getInstance().getBUDDY_ID_COUNT();
+				CalcButeco.getInstance().getBuddyList_DB()
+						.put(name, Integer.valueOf(ID_));
+				CalcButeco.getInstance().inc_BUDDY_ID_COUNT();
+
+				updateDisplayBuddyList();
+
+				return true;
+			}
+		}
+
+		return false;
+
+	}
+
+	private void WarnAlredyExists() {
+		Toast.makeText(getApplicationContext(), "Este cabra já tá na lista...",
+				Toast.LENGTH_SHORT).show();
+
+	}
+
+	private void updateDisplayBuddyList() {
+
+		StringBuilder tmp = new StringBuilder();
+
+		if (CalcButeco.getInstance().getBuddyList_DB() != null) {
+
+			for (Enumeration<String> en = CalcButeco.getInstance()
+					.getBuddyList_DB().keys(); en.hasMoreElements();) {
+				String name = en.nextElement();
+				int id = CalcButeco.getInstance().getBuddyList_DB().get(name);
+
+				tmp.append(name);
+				tmp.append("\t[" + id + "]");
+				tmp.append("\n");
+			}
+
+			buddyList.setText(tmp.toString());
+		}
+
+	}
+
+	private void showMsg(String text) {
+
+		Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT)
+				.show();
+
+	}
+
+}
---
+++ tags/ButecoCalc/src/com/calculadoradebuteco/model/CalcButeco.java
@@ -0,0 +1,113 @@
+package com.calculadoradebuteco.model;
+
+import java.util.Hashtable;
+
+import com.calculadoradebuteco.core.DataItem;
+
+public class CalcButeco {
+
+	private Hashtable<String, DataItem> itemList_DB = null;
+	private Hashtable<String, Integer> buddyList_DB = null;
+	private Hashtable<String, Integer> ItemIDs = null;
+	private int Matrix[][] = null;
+	private int BUDDY_ID_COUNT = 0;
+	private int ITEM_ID_COUNT = 0;
+
+	private static CalcButeco instance;
+
+	public static CalcButeco getInstance() {
+
+		if (instance == null) {
+			instance = new CalcButeco();
+		}
+
+		return instance;
+	}
+
+	public Hashtable<String, Integer> getItemIDs() {
+		return ItemIDs;
+	}
+
+	public void setItemIDs(Hashtable<String, Integer> itemIDs) {
+		ItemIDs = itemIDs;
+	}
+
+	public Hashtable<String, DataItem> getItemListDB() {
+		return itemList_DB;
+	}
+
+	public void setItemListDB(Hashtable<String, DataItem> itemListDB) {
+		this.itemList_DB = itemListDB;
+	}
+
+	public Hashtable<String, Integer> getBuddyList_DB() {
+		return buddyList_DB;
+	}
+
+	public void setBuddyList_DB(Hashtable<String, Integer> buddyList_DB) {
+		this.buddyList_DB = buddyList_DB;
+	}
+
+	public int getBUDDY_ID_COUNT() {
+		return BUDDY_ID_COUNT;
+	}
+
+	public void setBUDDY_ID_COUNT(int bUDDY_ID_COUNT) {
+		BUDDY_ID_COUNT = bUDDY_ID_COUNT;
+	}
+
+	public void inc_BUDDY_ID_COUNT() {
+		this.BUDDY_ID_COUNT++;
+	}
+
+	public void inc_ITEM_ID_COUNT() {
+		this.ITEM_ID_COUNT++;
+	}
+
+	public int getITEM_ID_COUNT() {
+		return ITEM_ID_COUNT;
+	}
+
+	public void setITEM_ID_COUNT(int iTEM_ID_COUNT) {
+		ITEM_ID_COUNT = iTEM_ID_COUNT;
+	}
+
+	public int[][] getMatrix() {
+		return Matrix;
+	}
+	
+	public int getMatrixData(String lin, String col) {
+		int l = ((Integer)(this.ItemIDs.get(lin)).intValue());
+		int c = ((Integer)(this.buddyList_DB.get(col)).intValue());
+		return Matrix[l][c];
+	}
+
+	public void setMatrix() {
+
+		if ((this.getItemListDB() != null) && (this.getBuddyList_DB() != null)) {
+			int item_size = this.getItemListDB().size();
+			int buddy_size = this.getBuddyList_DB().size();
+			Matrix = new int[item_size][buddy_size];
+
+			for (int x = 0; x < item_size; x++) {
+				for (int y = 0; y < buddy_size; y++) {
+					Matrix[x][y] = 0;
+				}
+			}
+		}
+	}
+
+	public void checkMatrix(int posItem, int posBuddy) {
+
+		this.Matrix[posItem][posBuddy] = 1;
+
+	}
+
+	public void uncheckMatrix(int posItem, int posBuddy) {
+
+		this.Matrix[posItem][posBuddy] = 0;
+
+	}
+
+}
+
