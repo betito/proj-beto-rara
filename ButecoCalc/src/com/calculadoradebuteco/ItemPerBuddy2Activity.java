6f24950f8b2ee1b4d89949eafefe80ee3e25e792
rnovellino@gmail.com <rnovellino@gmail.com@049d96ce-48fd-6cc9-8b01-b7ceffaa3ec7>
2013-08-02 17:32:09 +0000

    selecionando a lista de itens por buddy

--- ButecoCalc/src/com/calculadoradebuteco/ItemPerBuddy2Activity.java
+++ ButecoCalc/src/com/calculadoradebuteco/ItemPerBuddy2Activity.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.calculadoradebuteco.adapter.ListItemBuddyAdapter;
+import com.calculadoradebuteco.model.ItensListSelect;
 
 import android.app.Activity;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -20,6 +23,8 @@ import android.widget.TextView;
 public class ItemPerBuddy2Activity extends Activity implements OnItemClickListener {
 
 	private ListView buddiesListView;
+	
+	protected transient ListItemBuddyAdapter itemBuddyListAdapter;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -56,6 +61,26 @@ public class ItemPerBuddy2Activity extends Activity implements OnItemClickListen
 		
 		return list;
 	}
+	
+	/**
+	 * Prepare list of itens
+	 * @param iniString
+	 * @return
+	 */
+	private List<ItensListSelect> prepareListOfItens(final String iniString) {
+
+		List<ItensListSelect> list = new ArrayList<ItensListSelect>();
+		
+		ItensListSelect item;
+		
+		for (int i = 0; i < 10; i++) {
+			item = new ItensListSelect(iniString + i, false);
+			
+			list.add(item);
+		}
+		
+		return list;
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -69,12 +94,12 @@ public class ItemPerBuddy2Activity extends Activity implements OnItemClickListen
 	}
 	
 	
-
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
 		// open dialog
 		Dialog dialog = new Dialog(this);
+		dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
 		dialog.setContentView(R.layout.dialog_item_buddy);
 		
@@ -82,12 +107,37 @@ public class ItemPerBuddy2Activity extends Activity implements OnItemClickListen
 		desc.setText(String.format(getText(R.string.dialog_item_buddy_desc_text).toString(), id));
 		
 		ListView listView = (ListView) dialog.findViewById(R.id.dialog_item_buddy_list);
+		listView.setTextFilterEnabled(true);
 		
-		List<String> listItens = prepareListOfBuddies("Item ");
+		List<ItensListSelect> listItens = prepareListOfItens("Item ");
 		
-		listView.setAdapter(new ListItemBuddyAdapter(this, listItens));
+		itemBuddyListAdapter = new ListItemBuddyAdapter(this, listItens);
+		
+		listView.setAdapter(itemBuddyListAdapter);
+		
+		listView.setOnItemClickListener(new ItemBuddyItemClickListener());
 		
 		dialog.show();
+	}
+	
+	private class ItemBuddyItemClickListener implements OnItemClickListener {
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			
+			Log.e("CALC", "ItemBuddyItemClickListener::onItemClick");
+			
+			List<ItensListSelect> listItens = itemBuddyListAdapter.getList();
+			
+			final ItensListSelect item = new ItensListSelect(listItens.get(position).getItem(), (listItens.get(position).isSelected() ? false : true));
+			
+			listItens.set(position, item); 
+			
+			itemBuddyListAdapter.setList(listItens);
+			
+			itemBuddyListAdapter.notifyDataSetChanged();
+		}
+		
 	}
 
 }
