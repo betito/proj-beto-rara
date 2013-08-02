0b8d65a5bf24488528cf8307b4716e6926b84663
betito.oliveira@gmail.com <betito.oliveira@gmail.com@049d96ce-48fd-6cc9-8b01-b7ceffaa3ec7>
2013-08-02 21:47:07 +0000

    Fixes.

--- trunk/ButecoCalc/src/com/calculadoradebuteco/ItemPerBuddy2Activity.java
+++ trunk/ButecoCalc/src/com/calculadoradebuteco/ItemPerBuddy2Activity.java
@@ -1,10 +1,9 @@
 package com.calculadoradebuteco;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
 import java.util.List;
-
-import com.calculadoradebuteco.adapter.ListItemBuddyAdapter;
-import com.calculadoradebuteco.model.ItensListSelect;
 
 import android.app.Activity;
 import android.app.Dialog;
@@ -13,23 +12,33 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class ItemPerBuddy2Activity extends Activity implements OnItemClickListener {
+import com.calculadoradebuteco.adapter.ListItemBuddyAdapter;
+import com.calculadoradebuteco.model.CalcButeco;
+import com.calculadoradebuteco.model.ItensListSelect;
+
+public class ItemPerBuddy2Activity extends Activity implements OnClickListener,
+		OnItemClickListener {
 
 	private ListView buddiesListView;
-	
+
 	protected transient ListItemBuddyAdapter itemBuddyListAdapter;
+
+	private Button btn_clear = null;
+	private Button btn_close = null;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_item_per_buddy2);
 	}
 
@@ -37,48 +46,64 @@ public class ItemPerBuddy2Activity extends Activity implements OnItemClickListen
 	protected void onResume() {
 		super.onResume();
 
-//		fm = getSupportFragmentManager();
+		// fm = getSupportFragmentManager();
 
-		List<String> listOfBuddies = prepareListOfBuddies("Jovem ");
+		List<String> listOfBuddies = prepareListOfBuddies();
 
 		buddiesListView = (ListView) findViewById(R.id.itembuddy_list_of_buddies);
 
-		final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listOfBuddies);
+		final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+				this, android.R.layout.simple_list_item_1, listOfBuddies);
 
 		buddiesListView.setAdapter(arrayAdapter);
 
 		buddiesListView.setOnItemClickListener(this);
 
+		btn_clear = (Button) findViewById(R.id.btn_clear);
+		btn_close = (Button) findViewById(R.id.btn_save);
+
+		btn_clear.setOnClickListener(this);
+		btn_close.setOnClickListener(this);
+
 	}
 
-	private List<String> prepareListOfBuddies(final String iniString) {
+	private List<String> prepareListOfBuddies() {
 
 		List<String> list = new ArrayList<String>();
+		Hashtable<String, Integer> names = CalcButeco.getInstance()
+				.getBuddyList_DB();
 
-		for (int i = 0; i < 10; i++) {
-			list.add(iniString + i);
+		for (Enumeration<String> en = names.keys(); en.hasMoreElements();) {
+			String name = en.nextElement();
+			list.add(name);
 		}
-		
+
 		return list;
 	}
-	
+
 	/**
 	 * Prepare list of itens
+	 * 
 	 * @param iniString
 	 * @return
 	 */
-	private List<ItensListSelect> prepareListOfItens(final String iniString) {
+	private List<ItensListSelect> prepareListOfItem() {
 
 		List<ItensListSelect> list = new ArrayList<ItensListSelect>();
-		
+
 		ItensListSelect item;
-		
-		for (int i = 0; i < 10; i++) {
-			item = new ItensListSelect(iniString + i, false);
-			
+
+		Hashtable<String, Integer> names = CalcButeco.getInstance()
+				.getItemIDs();
+
+		for (Enumeration<String> en = names.keys(); en.hasMoreElements();) {
+			String iniString = en.nextElement();
+
+			item = new ItensListSelect(iniString, false);
+
 			list.add(item);
 		}
-		
+
 		return list;
 	}
 
@@ -92,52 +117,89 @@ public class ItemPerBuddy2Activity extends Activity implements OnItemClickListen
 	public boolean onOptionsItemSelected(MenuItem item) {
 		return super.onOptionsItemSelected(item);
 	}
-	
-	
+
 	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
 
 		// open dialog
 		Dialog dialog = new Dialog(this);
 		dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		
+
 		dialog.setContentView(R.layout.dialog_item_buddy);
-		
-		TextView desc = (TextView) dialog.findViewById(R.id.dialog_item_buddy_desc);
-		desc.setText(String.format(getText(R.string.dialog_item_buddy_desc_text).toString(), id));
-		
-		ListView listView = (ListView) dialog.findViewById(R.id.dialog_item_buddy_list);
+
+		TextView name = (TextView) view;
+
+		TextView desc = (TextView) dialog
+				.findViewById(R.id.dialog_item_buddy_desc);
+		desc.setText(String.format(
+				getText(R.string.dialog_item_buddy_desc_text).toString(), name
+						.getText().toString()));
+
+		ListView listView = (ListView) dialog
+				.findViewById(R.id.dialog_item_buddy_list);
+
 		listView.setTextFilterEnabled(true);
-		
-		List<ItensListSelect> listItens = prepareListOfItens("Item ");
-		
+
+		List<ItensListSelect> listItens = prepareListOfItem();
+
 		itemBuddyListAdapter = new ListItemBuddyAdapter(this, listItens);
-		
+
 		listView.setAdapter(itemBuddyListAdapter);
-		
+
 		listView.setOnItemClickListener(new ItemBuddyItemClickListener());
-		
+
 		dialog.show();
 	}
-	
+
 	private class ItemBuddyItemClickListener implements OnItemClickListener {
 
 		@Override
-		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-			
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+
 			Log.e("CALC", "ItemBuddyItemClickListener::onItemClick");
-			
+
 			List<ItensListSelect> listItens = itemBuddyListAdapter.getList();
-			
-			final ItensListSelect item = new ItensListSelect(listItens.get(position).getItem(), (listItens.get(position).isSelected() ? false : true));
-			
-			listItens.set(position, item); 
-			
+
+			final ItensListSelect item = new ItensListSelect(listItens.get(
+					position).getItem(),
+					(listItens.get(position).isSelected() ? false : true));
+
+			listItens.set(position, item);
+
 			itemBuddyListAdapter.setList(listItens);
-			
+
 			itemBuddyListAdapter.notifyDataSetChanged();
 		}
-		
+
 	}
 
+	@Override
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+
+		case R.id.btn_clear:
+			Clear();
+			break;
+		case R.id.btn_save:
+			SaveAndClose();
+			break;
+
+		}
+
+	}
+
+	private void SaveAndClose() {
+
+		Log.i("BOTAO", "Salvar e Fechar.");
+
+	}
+
+	private void Clear() {
+
+		Log.i("BOTAO", "Desmarcar todos.");
+
+	}
 }
--- trunk/ButecoCalc/src/com/calculadoradebuteco/adapter/ListItemBuddyAdapter.java
+++ trunk/ButecoCalc/src/com/calculadoradebuteco/adapter/ListItemBuddyAdapter.java
@@ -17,14 +17,15 @@ public class ListItemBuddyAdapter extends BaseAdapter {
 
 	private final LayoutInflater inflater;
 	private transient List<ItensListSelect> list;
-	
+
 	public ListItemBuddyAdapter(Context context, List<ItensListSelect> list) {
-		
-		inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		
+
+		inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
 		this.list = list;
 	}
-	
+
 	@Override
 	public int getCount() {
 		return list.size();
@@ -42,36 +43,38 @@ public class ListItemBuddyAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, final View convertView, ViewGroup parent) {
-		
+
 		View view = convertView;
-		
-		if(convertView == null) {
-			
-			view = inflater.inflate(R.layout.item_list_item_buddy, parent, false);
+
+		if (convertView == null) {
+
+			view = inflater.inflate(R.layout.item_list_item_buddy, parent,
+					false);
 		}
-		
+
 		ItensListSelect item = getItem(position);
-		
-		TextView itemName = (TextView) view.findViewById(R.id.item_list_item_buddy_desc);
-		
+
+		TextView itemName = (TextView) view
+				.findViewById(R.id.item_list_item_buddy_desc);
+
 		itemName.setText(item.getItem());
-		
+
 		int bgColor;
-		if(item.isSelected()) {
+		if (item.isSelected()) {
 			bgColor = Color.parseColor("#00775F");
-		}
-		else {
+		} else {
 			bgColor = Color.parseColor("#00779F");
 		}
-		
+
 		view.setBackgroundColor(bgColor);
-		
+
 		return view;
 	}
-	
+
 	public void setList(List<ItensListSelect> list) {
 		this.list = list;
 	}
+
 	public List<ItensListSelect> getList() {
 		return list;
 	}
