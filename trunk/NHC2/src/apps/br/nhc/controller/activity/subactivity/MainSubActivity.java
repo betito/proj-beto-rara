995c99b318ef6e92c016612ce7c3832656bba1cd
betito.oliveira@gmail.com <betito.oliveira@gmail.com@049d96ce-48fd-6cc9-8b01-b7ceffaa3ec7>
2013-08-23 22:13:56 +0000

    Bill output format to show 2 decimals.

--- trunk/NHC2/src/apps/br/nhc/controller/activity/subactivity/MainSubActivity.java
+++ trunk/NHC2/src/apps/br/nhc/controller/activity/subactivity/MainSubActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import apps.br.nhc.R;
@@ -17,43 +17,55 @@ import apps.br.nhc.controller.activity.MainActivity;
 import apps.br.nhc.model.domain.PersonItem;
 import apps.br.nhc.view.adapter.ListBuddyAdapter;
 
-public class MainSubActivity extends SubActivity implements OnClickListener, OnItemClickListener {
+public class MainSubActivity extends SubActivity implements OnClickListener,
+		OnItemClickListener {
 
 	private TextView txvTotal;
 	private TextView txvTotalP10;
-	
+
 	// adapter of person list
 	private ListBuddyAdapter listItemBuddyAdapter;
-	
+
 	public MainSubActivity(final Activity activity, final View viewRootSub) {
 		super(activity, viewRootSub);
-		
+
 		txvTotal = (TextView) activity.findViewById(R.id.main_screen_total);
-		txvTotalP10 = (TextView) activity.findViewById(R.id.main_screen_plus_10);
-		
-		ListView personList = (ListView) activity.findViewById(R.id.main_screen_people_list);
-		Button addPersonButton = (Button) activity.findViewById(R.id.main_screen_add_person_button);
-		Button addItemButton = (Button) activity.findViewById(R.id.main_screen_add_item_button);
-		
+		txvTotalP10 = (TextView) activity
+				.findViewById(R.id.main_screen_plus_10);
+
+		ListView personList = (ListView) activity
+				.findViewById(R.id.main_screen_people_list);
+		ImageButton addPersonButton = (ImageButton) activity
+				.findViewById(R.id.main_screen_add_person_button);
+		ImageButton addItemButton = (ImageButton) activity
+				.findViewById(R.id.main_screen_add_item_button);
+
 		addPersonButton.setOnClickListener(this);
 		addItemButton.setOnClickListener(this);
-		
+
 		// get list of person
-		List<PersonItem> listPerson = Arrays.asList(NhcBO.getInstance().getBuddyListDB().values().toArray(new PersonItem[NhcBO.getInstance().getBuddyListDB().values().size()]));
-		
-		personList.setAdapter(listItemBuddyAdapter = new ListBuddyAdapter(activity, listPerson));
-		
+		List<PersonItem> listPerson = Arrays.asList(NhcBO
+				.getInstance()
+				.getBuddyListDB()
+				.values()
+				.toArray(
+						new PersonItem[NhcBO.getInstance().getBuddyListDB()
+								.values().size()]));
+
+		personList.setAdapter(listItemBuddyAdapter = new ListBuddyAdapter(
+				activity, listPerson));
+
 		personList.setOnItemClickListener(this);
 	}
-	
+
 	@Override
 	public void onClick(final View view) {
-		
+
 		switch (view.getId()) {
 		case R.id.main_screen_add_person_button:
 			((MainActivity) activity).openAddPersonScreen();
 			break;
-			
+
 		case R.id.main_screen_add_item_button:
 			((MainActivity) activity).openAddItemScreen();
 			break;
@@ -61,33 +73,43 @@ public class MainSubActivity extends SubActivity implements OnClickListener, OnI
 		default:
 			break;
 		}
-		
+
 	}
-	
+
 	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+
 		TextView txvName = (TextView) view.findViewById(R.id.person_list_name);
-		
+
 		((MainActivity) activity).openMergeScreen(txvName.getText().toString());
-		
+
 	}
-	
+
 	public void notifyChangeList() {
 		// get list of person
-		List<PersonItem> listPerson = Arrays.asList(NhcBO.getInstance().getBuddyListDB().values().toArray(new PersonItem[NhcBO.getInstance().getBuddyListDB().values().size()]));
-		
+		List<PersonItem> listPerson = Arrays.asList(NhcBO
+				.getInstance()
+				.getBuddyListDB()
+				.values()
+				.toArray(
+						new PersonItem[NhcBO.getInstance().getBuddyListDB()
+								.values().size()]));
+
 		listItemBuddyAdapter.setList(listPerson);
-		
+
 		listItemBuddyAdapter.notifyDataSetChanged();
 	}
-	
+
 	public void updateTotals() {
-		
-		txvTotal.setText(" = " + String.valueOf(NhcBO.getInstance().getTotalBill()));
-		txvTotalP10.setText(" = " + String.valueOf(NhcBO.getInstance().getTotalP10()));
-		
+
+		txvTotal.setText(" = "
+				+ String.format("%.2f",
+						NhcBO.getInstance().getTotalBill()));
+		txvTotalP10.setText(" = "
+				+ String.format("%.2f",
+						NhcBO.getInstance().getTotalP10()));
+
 	}
-	
-	
+
 }
