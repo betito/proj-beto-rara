c8785fdaa9d087ebd3edc12607bd6e40ad49aa26
betito.oliveira@gmail.com <betito.oliveira@gmail.com@049d96ce-48fd-6cc9-8b01-b7ceffaa3ec7>
2013-09-13 21:25:01 +0000

    Request focus on text input.

--- trunk/NHC2/src/apps/br/nhc/controller/activity/subactivity/AddPersonSubActivity.java
+++ trunk/NHC2/src/apps/br/nhc/controller/activity/subactivity/AddPersonSubActivity.java
@@ -1,8 +1,10 @@
 package apps.br.nhc.controller.activity.subactivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import apps.br.nhc.R;
@@ -11,27 +13,42 @@ import apps.br.nhc.controller.activity.MainActivity;
 import apps.br.nhc.controller.util.Utils;
 import apps.br.nhc.model.domain.PersonItem;
 
-public class AddPersonSubActivity extends SubActivity implements OnClickListener {
-	
+public class AddPersonSubActivity extends SubActivity implements
+		OnClickListener {
+
 	private EditText edtName;
 	private Button butCancel;
 	private Button butSave;
-	
+
 	public AddPersonSubActivity(Activity activity, final View viewRootSub) {
 		super(activity, viewRootSub);
-		
+
 		edtName = (EditText) activity.findViewById(R.id.add_person_edit_name);
-		butCancel = (Button) activity.findViewById(R.id.add_person_button_cancel);
+		butCancel = (Button) activity
+				.findViewById(R.id.add_person_button_cancel);
 		butSave = (Button) activity.findViewById(R.id.add_person_button_save);
-		
+
 		butCancel.setOnClickListener(this);
 		butSave.setOnClickListener(this);
-		
+
+		focusOn(edtName);
+
+	}
+
+	private void focusOn(EditText edt) {
+
+		edt.requestFocus();
+
+		InputMethodManager keyboard = (InputMethodManager) activity
+				.getApplicationContext().getSystemService(
+						Context.INPUT_METHOD_SERVICE);
+		keyboard.showSoftInput(edt, 0);
+
 	}
 
 	@Override
 	public void onClick(View view) {
-		
+
 		switch (view.getId()) {
 		case R.id.add_person_button_cancel:
 			((MainActivity) activity).closeAddPersonAct(viewRootSub);
@@ -44,22 +61,25 @@ public class AddPersonSubActivity extends SubActivity implements OnClickListener
 		default:
 			break;
 		}
-		
+
 	}
-	
+
 	private void savePerson() {
-		
-		String name = edtName.getText().toString().trim().toUpperCase(Utils.getLocale());
-		
-		if(!name.isEmpty() && !NhcBO.getInstance().getBuddyListDB().containsKey(name)) {
+
+		String name = edtName.getText().toString().trim()
+				.toUpperCase(Utils.getLocale());
+
+		if (!name.isEmpty()
+				&& !NhcBO.getInstance().getBuddyListDB().containsKey(name)) {
 			PersonItem item = new PersonItem(name, 0d, 0d);
-			
+
 			NhcBO.getInstance().getBuddyListDB().put(name, item);
+			
+			focusOn(edtName);
+			
 		}
-		
+
 		((MainActivity) activity).closeAddPersonAct(viewRootSub);
 	}
-	
-	
 
 }
