f904ff66e89802205123a4a5fbe64d576c10e49d
betito.oliveira@gmail.com <betito.oliveira@gmail.com@049d96ce-48fd-6cc9-8b01-b7ceffaa3ec7>
2013-09-13 21:25:01 +0000

    Request focus on text input.

--- NHC2/src/apps/br/nhc/controller/activity/subactivity/AddItemSubActivity.java
+++ NHC2/src/apps/br/nhc/controller/activity/subactivity/AddItemSubActivity.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -23,33 +25,55 @@ public class AddItemSubActivity extends SubActivity implements OnClickListener {
 	private EditText edtQtd;
 	private Button butCancel;
 	private Button butSave;
-	
+
 	private ListView listItem;
-	
+
 	private ListItemAdapter listItemAdapter;
-	
+
 	public AddItemSubActivity(Activity activity, View viewRootSub) {
 		super(activity, viewRootSub);
-		
+
 		edtName = (EditText) activity.findViewById(R.id.add_item_edit_name);
-		edtPrice = (EditText) activity.findViewById(R.id.add_item_edit_unit_price);
+		edtPrice = (EditText) activity
+				.findViewById(R.id.add_item_edit_unit_price);
 		edtQtd = (EditText) activity.findViewById(R.id.add_item_edit_qtd);
 		butCancel = (Button) activity.findViewById(R.id.add_item_button_cancel);
 		butSave = (Button) activity.findViewById(R.id.add_item_button_save);
 		listItem = (ListView) activity.findViewById(R.id.add_item_list);
-		
+
 		butCancel.setOnClickListener(this);
 		butSave.setOnClickListener(this);
-		
+
 		// get list of itens
-		List<DataItem> list = Arrays.asList(NhcBO.getInstance().getItemListDB().values().toArray(new DataItem[NhcBO.getInstance().getItemListDB().values().size()]));
-		
-		listItem.setAdapter(listItemAdapter = new ListItemAdapter(activity, list));
+		List<DataItem> list = Arrays.asList(NhcBO
+				.getInstance()
+				.getItemListDB()
+				.values()
+				.toArray(
+						new DataItem[NhcBO.getInstance().getItemListDB()
+								.values().size()]));
+
+		listItem.setAdapter(listItemAdapter = new ListItemAdapter(activity,
+				list));
+
+		focusOn(edtName);
+
+	}
+
+	private void focusOn(EditText edt) {
+
+		edt.requestFocus();
+
+		InputMethodManager keyboard = (InputMethodManager) activity
+				.getApplicationContext().getSystemService(
+						Context.INPUT_METHOD_SERVICE);
+		keyboard.showSoftInput(edt, 0);
+
 	}
 
 	@Override
 	public void onClick(View view) {
-		
+
 		switch (view.getId()) {
 		case R.id.add_item_button_cancel:
 			((MainActivity) activity).closeAddItemAct(viewRootSub);
@@ -62,48 +86,52 @@ public class AddItemSubActivity extends SubActivity implements OnClickListener {
 		default:
 			break;
 		}
-		
+
 	}
-	
+
 	private void saveItem() {
-		
-		String name = edtName.getText().toString().trim().toUpperCase(Utils.getLocale());
+
+		String name = edtName.getText().toString().trim()
+				.toUpperCase(Utils.getLocale());
 		String price = edtPrice.getText().toString().trim();
 		String quant = edtQtd.getText().toString().trim();
-		
-		if(NhcBO.getInstance().getBuddyListDB().containsKey(name)) {
+
+		if (NhcBO.getInstance().getBuddyListDB().containsKey(name)) {
 			// TODO: make warning user
-		}
-		else if(name.isEmpty()) {
+		} else if (name.isEmpty()) {
 			// TODO: make warning user
-		}
-		else if(price.isEmpty()) {
+		} else if (price.isEmpty()) {
 			// TODO: make warning user
-		}
-		else if(quant.isEmpty()) {
+		} else if (quant.isEmpty()) {
 			// TODO: make warning user
-		}
-		else {
-			
+		} else {
+
 			int id = NhcBO.getInstance().getItemListDB().size();
-			
+
 			DataItem item = new DataItem(id, name, price, quant);
-			
+
 			NhcBO.getInstance().getItemListDB().put(name, item);
-			
+
 			// get list of itens
-			List<DataItem> list = Arrays.asList(NhcBO.getInstance().getItemListDB().values().toArray(new DataItem[NhcBO.getInstance().getItemListDB().values().size()]));
-			
+			List<DataItem> list = Arrays.asList(NhcBO
+					.getInstance()
+					.getItemListDB()
+					.values()
+					.toArray(
+							new DataItem[NhcBO.getInstance().getItemListDB()
+									.values().size()]));
+
 			listItemAdapter.setList(list);
-			
+
 			listItemAdapter.notifyDataSetChanged();
-			
-			
-			edtName.setText("");
+
 			edtPrice.setText("");
 			edtQtd.setText("");
+			edtName.setText("");
+			focusOn(edtName);
+
 		}
-		
+
 	}
 
 }
