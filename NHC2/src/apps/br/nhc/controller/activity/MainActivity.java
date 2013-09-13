66fef1a8a90ee31be99a346abdd61bfbddea0b0f
betito.oliveira@gmail.com <betito.oliveira@gmail.com@049d96ce-48fd-6cc9-8b01-b7ceffaa3ec7>
2013-09-13 21:23:25 +0000

    Format.

--- NHC2/src/apps/br/nhc/controller/activity/MainActivity.java
+++ NHC2/src/apps/br/nhc/controller/activity/MainActivity.java
@@ -12,6 +12,7 @@ import apps.br.nhc.R;
 import apps.br.nhc.controller.NhcBO;
 import apps.br.nhc.controller.activity.subactivity.AddItemSubActivity;
 import apps.br.nhc.controller.activity.subactivity.AddPersonSubActivity;
+import apps.br.nhc.controller.activity.subactivity.CheckSubActivity;
 import apps.br.nhc.controller.activity.subactivity.MainSubActivity;
 import apps.br.nhc.controller.activity.subactivity.MergeSubActivity;
 
@@ -21,104 +22,122 @@ public class MainActivity extends Activity {
 	private AddPersonSubActivity addPersonSubActivity;
 	private AddItemSubActivity addItemSubActivity;
 	private MergeSubActivity addItemPersonSubActivity;
-	
+	private CheckSubActivity checkSubActivity;
+
 	// layout inflater
 	private LayoutInflater inflater;
-	
+
 	// view root
 	private ViewGroup viewRoot;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_main);
-		
+
 		inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		
+
 		viewRoot = (ViewGroup) getWindow().getDecorView().getRootView();
-		
-		mainSubActivity = new MainSubActivity(this, getWindow().getDecorView().getRootView());
-		
+
+		mainSubActivity = new MainSubActivity(this, getWindow().getDecorView()
+				.getRootView());
+
 	}
-	
+
 	public void openAddPersonScreen() {
-		
-		View viewRootAddPerson = inflater.inflate(R.layout.add_person_screen, viewRoot, false);
-		
+
+		View viewRootAddPerson = inflater.inflate(R.layout.add_person_screen,
+				viewRoot, false);
+
 		viewRoot.addView(viewRootAddPerson);
-		
+
 		addPersonSubActivity = new AddPersonSubActivity(this, viewRootAddPerson);
 	}
-	
+
 	public void openAddItemScreen() {
-		
-		View viewRootAddItem = inflater.inflate(R.layout.add_item_screen, viewRoot, false);
-		
+
+		View viewRootAddItem = inflater.inflate(R.layout.add_item_screen,
+				viewRoot, false);
+
 		viewRoot.addView(viewRootAddItem);
-		
+
 		addItemSubActivity = new AddItemSubActivity(this, viewRootAddItem);
 	}
-	
-	public void openMergeScreen(final String name) {
-		
-		View viewRootItemPerson = inflater.inflate(R.layout.merge_screen, viewRoot, false);
-		
-		viewRoot.addView(viewRootItemPerson);
-		
-		addItemPersonSubActivity = new MergeSubActivity(this, viewRootItemPerson, name);
+
+	public void openItemQuantityScreen() {
+
+		View viewRootAddItem = inflater.inflate(R.layout.qtt_item_screen,
+				viewRoot, false);
+
+		viewRoot.addView(viewRootAddItem);
+
+		checkSubActivity = new CheckSubActivity(this, viewRootAddItem);
 	}
-	
+
+	public void openMergeScreen(final String name) {
+
+		View viewRootItemPerson = inflater.inflate(R.layout.merge_screen,
+				viewRoot, false);
+
+		viewRoot.addView(viewRootItemPerson);
+
+		addItemPersonSubActivity = new MergeSubActivity(this,
+				viewRootItemPerson, name);
+	}
+
 	public void closeAddPersonAct(final View viewRootSub) {
 		closeSubActivity(viewRootSub);
-		
+
 		addPersonSubActivity = null;
 	}
-	
+
 	public void closeAddItemAct(final View viewRootSub) {
 		closeSubActivity(viewRootSub);
-		
+
 		addItemSubActivity = null;
 	}
-	
+
 	public void closeMergeAct(final View viewRootSub) {
-		
+
 		// do calcs
 		showCheck();
-		
+
 		closeSubActivity(viewRootSub);
 	}
-	
+
 	private void showCheck() {
 
 		if (NhcBO.getInstance().getMatrix() != null) {
-			
+
 			Log.i("MATRIX", "OK");
-			
+
 			NhcBO.getInstance().calcBill();
-			
+
 			mainSubActivity.notifyChangeList();
-			
+
 			mainSubActivity.updateTotals();
 		}
 
 	}
-	
+
 	/**
 	 * Closes a sub activity
-	 * @param viewRootSub - root view of the sub activity
+	 * 
+	 * @param viewRootSub
+	 *            - root view of the sub activity
 	 */
 	private void closeSubActivity(final View viewRootSub) {
 		mainSubActivity.notifyChangeList();
-		
+
 		// remove view from viewgroup root
 		viewRoot.removeView(viewRootSub);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.main, menu);
+		// getMenuInflater().inflate(R.menu.main, menu);
 		return false;
 	}
 
